5639b538bfe50b29330d67993ba741612360a8ab
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-25 15:29:49 +0300

    Variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+// 4
+var name = "Mohamed Ibrahim";
+
+var age;
+age = 26;
+
+var friends = ["Ali", "Kareem"];
+
+console.log(friends.length); // 2
+console.log(friends[1]); // Kareem
+
+
+age++; // 27
+age += 2;
+
+console.log("🚀 ~ age:", age); // 29
