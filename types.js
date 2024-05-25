a21912f86d57bd3324068b01ec8dcae021730ea1
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-25 14:42:45 +0300

    Types

---
+++ types.js
@@ -0,0 +1,13 @@
+// 3
+console.log(typeof 42); // "number"
+
+console.log(typeof "mohamed"); // "string"
+
+console.log(typeof true); // "boolean"
+
+console.log(typeof undefined); // "undefined"
+
+console.log(typeof { age: 26 }); // "object"
+
+console.log(typeof null); // "object" !?!?!?
+console.log(typeof [1, 2, 3]); // "object"
