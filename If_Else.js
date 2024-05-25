7fca8dce04f17d47b6fe326150394fb8ea47ae79
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-25 16:32:29 +0300

    If & Else

---
+++ If_Else.js
@@ -0,0 +1,17 @@
+var age = 26;
+
+if (age >= 18) {
+  // Do something
+  // getVote();
+} else {
+  // do something else
+}
+
+
+var isEnrolled = true;
+
+if (isEnrolled) {
+  console.log("Take the class!");
+} else {
+  console.log("Enroll first");
+}
