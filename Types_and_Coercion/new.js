90bd9e58053e457367db225870cd20e9b40866a8
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-27 09:17:17 +0300

    new keyword

---
+++ Types_and_Coercion/new.js
@@ -0,0 +1,8 @@
+var yesterday = new Date("March 6, 2019");
+
+console.log(yesterday.toUTCString());
+// Tue, 05 Mar 2019 22:00:00 GMT
+
+var myGPA = String(2.85);
+console.log(myGPA); // "2.85"
+console.log(typeof myGPA); // string
