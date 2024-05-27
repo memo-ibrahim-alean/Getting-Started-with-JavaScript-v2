91ad0422d9343242348d209920c00cdc1831d446
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-27 09:27:55 +0300

    Coercion

---
+++ Types_and_Coercion/Coercion.js
@@ -0,0 +1,12 @@
+var msg1 = "There are ";
+
+var numStudents = 16;
+
+var msg2 = " students.";
+
+console.log(msg1 + numStudents + msg2);
+// There are 16 students.
+
+console.log(
+  `There are ${numStudents} students.`
+);
